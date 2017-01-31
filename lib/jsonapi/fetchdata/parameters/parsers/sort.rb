11743335c250535db33f3cc9cb0c1d7354760576
Michel Pouleijn <m.pouleijn@gynzy.com>
2017-01-31 23:37:07 +0100

    Added rails check for the sort param

--- lib/jsonapi/fetchdata/parameters/parsers/sort.rb
+++ lib/jsonapi/fetchdata/parameters/parsers/sort.rb
@@ -8,8 +8,11 @@ module JSONAPI
 
           def parse params
             if params && (params.size > 0)
-              # [params].flatten.map{ |col| col.scan(/[-\w]+/)}.flatten.map { |c| (c[0] == '-') ? Hash[c[1..-1], 'desc'] : c }
-              params.scan(/[-\w]+/).flatten.map { |c| (c[0] == '-') ? "#{c[1..-1]} DESC" : c }.join(", ")
+              if Rails::VERSION::MAJOR >= 4
+                [params].flatten.map{ |col| col.scan(/[-\w]+/)}.flatten.map { |c| (c[0] == '-') ? Hash[c[1..-1], 'desc'] : c }
+              else
+                params.scan(/[-\w]+/).flatten.map { |c| (c[0] == '-') ? "#{c[1..-1]} DESC" : c }.join(", ")
+              end
             end
           end
 
